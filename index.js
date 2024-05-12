927f2c09110278afbd5f683ed15c407716f382c6
Jesse Schulman <jesse@caleos.io>
2024-05-11 21:41:40 -0700

    Adding Antelope as export of bundle

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ import { WebRenderer } from "@wharfkit/web-renderer"
 import { WalletPluginCloudWallet } from "@wharfkit/wallet-plugin-cloudwallet"
 import { WalletPluginAnchor } from "@wharfkit/wallet-plugin-anchor"
 import { WalletPluginWombat } from "@wharfkit/wallet-plugin-wombat"
+import * as Antelope from "@wharfkit/antelope"
 
 async function main() {
   window.Wharfkit = {
+    Antelope,
     SessionKit,
     WebRenderer,
     WalletPluginCloudWallet,
